2c46c7486fc053df034f6a631cce7ba51c5a7075
Ani Ravi <5902976+aniravi24@users.noreply.github.com>
2019-08-06 17:28:49 -0700

    account for different windows

--- background.js
+++ background.js
@@ -1,37 +1,72 @@
-// [0] refers to the actual time value
-// [1] refers to the setinterval function for that tab
+// tabs data structure is { id: [minutes, seconds, setinterval] }
+// windows data structure is { id: lastActiveTabId }
 
-const timeSpent = {};
-let activeTabId;
 chrome.browserAction.setBadgeBackgroundColor({ color: "black" });
-function onUpdatedListener(tabId, changeInfo, tab) {
+const tabs = {};
+const windows = {};
+
+let activeTabId;
+let activeWindowId;
+
+function timeTrack() {
+  tabs[activeTabId][1] += 1;
+  if (tabs[activeTabId][1] >= 60) {
+    tabs[activeTabId][1] = 0;
+    tabs[activeTabId][0] += 1;
+    chrome.browserAction.setBadgeText({
+      text: tabs[activeTabId][0] + "m"
+    });
+  }
+}
+
+function onUpdatedTabListener(tabId, changeInfo, tab) {
   chrome.tabs.get(tabId.tabId, function(tab) {
     // when previous tab is changed
-    if (timeSpent[activeTabId]) {
-      clearInterval(timeSpent[activeTabId][1]);
+    if (tabs[activeTabId]) {
+      clearInterval(tabs[activeTabId][2]);
     }
     // active tab
     activeTabId = tab.id;
-    if (!timeSpent[activeTabId]) {
-      timeSpent[activeTabId] = [0, 0];
+    if (!tabs[activeTabId]) {
+      tabs[activeTabId] = [0, 0, 0];
       chrome.browserAction.setBadgeText({ text: "0m" });
     } else {
       chrome.browserAction.setBadgeText({
-        text: timeSpent[activeTabId][0] + "m"
+        text: tabs[activeTabId][0] + "m"
       });
     }
     // start tracking again
-    timeSpent[activeTabId][1] = setInterval(
-      timeTrack.bind(timeTrack),
-      1 * 60 * 1000
-    );
+    clearInterval(tabs[activeTabId][2]);
+    tabs[activeTabId][2] = setInterval(timeTrack.bind(timeTrack), 1000);
   });
 }
-// Subscribe to tab events
-chrome.tabs.onActivated.addListener(onUpdatedListener);
-// End tabs listeners-----------
 
-function timeTrack() {
-  timeSpent[activeTabId][0] += 1;
-  chrome.browserAction.setBadgeText({ text: timeSpent[activeTabId][0] + "m" });
+function windowFocusChangedListener(windowId) {
+  // when window is not focused
+  if (windowId == chrome.windows.WINDOW_ID_NONE) {
+    if (tabs[activeTabId]) {
+      clearInterval(tabs[activeTabId][2]);
+    }
+  } else {
+    if (windowId !== activeWindowId) {
+      if (activeTabId) windows[activeWindowId] = activeTabId;
+      if (tabs[activeTabId]) clearInterval(tabs[activeTabId][2]);
+      if (windows[windowId]) {
+        activeTabId = windows[windowId];
+      } else {
+        activeTabId = undefined;
+      }
+      activeWindowId = windowId;
+    }
+    if (tabs[activeTabId]) {
+      clearInterval(tabs[activeTabId][2]);
+      tabs[activeTabId][2] = setInterval(timeTrack.bind(timeTrack), 1000);
+    }
+  }
 }
+
+// subscribe to window focus change events
+chrome.windows.onFocusChanged.addListener(windowFocusChangedListener);
+
+// Subscribe to tab events
+chrome.tabs.onActivated.addListener(onUpdatedTabListener);
